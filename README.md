3498e5bdc3c1840a28d61448d472634fd1ffb800
linjuming1981 <33610987+linjuming1981@users.noreply.github.com>
2023-10-13 17:10:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gpt
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/linjuming1981/gpt)
---
+++ diary.js
@@ -0,0 +1,11 @@
+娱乐大平台数据：
+https://www.zhihu.com/question/54520089
+
+
+热度：
+  新上演电影票房排行榜
+  https://ys.endata.cn/DataMarket/Index
+
+电影排行榜思路：
+  用接口获取循环获取每一天的数据
+  
